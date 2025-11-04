a15ef72d14354c259e548315a414bc43407cb173
cfrotaru <c.f.rotaru@gmail.com>
2025-11-04 22:20:50 +0200

    feat (client): Create not found page having a button to go to home page

--- DatingApp/client/src/app/app.routes.ts
+++ DatingApp/client/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { NotFoundComponent } from './errrors/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -21,5 +22,6 @@ export const routes: Routes = [
     ],
   },
   { path: 'errors', component: TestErrorsComponent },
+  { path: 'not-found', component: NotFoundComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
